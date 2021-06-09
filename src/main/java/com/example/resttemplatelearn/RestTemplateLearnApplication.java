daf2088329f56d0e4ac5fb1a270f05d97e8de842
zhouhui <2522384355@qq.com>
2021-06-09 15:33:41 +0800

    init

---
+++ src/main/java/com/example/resttemplatelearn/RestTemplateLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.resttemplatelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateLearnApplication.class, args);
+    }
+
+}
